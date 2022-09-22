b5f79068c2acffdc186a6208d28d7dc49f846804
Anton Loshmanov <pppkms2339@gmail.com>
2022-09-22 20:51:29 +1000

    Поиск в BST, обход в глубину итеративный и рекурсивный

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+        Tree tree = new Tree();
+        tree.insert(10);
+        tree.insert(20);
+        tree.insert(8);
+        tree.pass();
+        tree.passIterative();
     }
 
 }
