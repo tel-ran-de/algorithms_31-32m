85ff37611252b6cc2a18b525e145aeb782869b62
Anton Loshmanov <pppkms2339@gmail.com>
2022-09-29 22:58:47 +1000

    Метод "Разделяй и властвуй". Quick Sort

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,49 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        Tree tree = new Tree();
-        tree.insert(10);
-        tree.insert(20);
-        tree.insert(8);
-        tree.insert(7);
-//        tree.pass();
-//        tree.passIterative();
-        tree.passWidth();
+        System.out.println(sum(new int[]{6, 4, 6}, 0));
+
+        int[] array = {39, 22, 2, 55, 6, 20};
+        qsort(array, 0, array.length - 1);
+        System.out.println(Arrays.toString(array));
     }
+
+    private static void qsort(int[] arr, int begin, int end) {
+        if (begin < end) {
+            int pivotIndex = partition(arr, begin, end);
+            qsort(arr, begin, pivotIndex - 1);
+            qsort(arr, pivotIndex + 1, end);
+        }
+    }
+
+    private static int partition(int[] arr, int begin, int end) {
+        int pivot = arr[end];
+        int pivotIndex = begin;
+        for (int i = begin; i < end; i++) {
+            if (arr[i] < pivot) {
+                swap(arr, i, pivotIndex);
+                pivotIndex++;
+            }
+        }
+        swap(arr, pivotIndex, end);
+        return pivotIndex;
+    }
+
+    private static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    private static int sum(int[] arr, int index) {
+        if (index == arr.length - 1) {
+            return arr[index];
+        }
+        return arr[index] + sum(arr, index + 1);
+    }
+
+
 
 }
