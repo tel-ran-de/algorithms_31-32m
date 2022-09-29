7aa32e145c93e4c6671417592b59a7ec4489b336
Anton Loshmanov <pppkms2339@gmail.com>
2022-09-29 18:37:10 +1000

    Обход дерева в ширину (2)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        Tree tree = new Tree();
+        tree.insert(10);
+        tree.insert(20);
+        tree.insert(8);
+        tree.insert(7);
+//        tree.pass();
+//        tree.passIterative();
+        tree.passWidth();
     }
 
 }
